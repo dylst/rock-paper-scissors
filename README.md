e561d0ac53fa14222ac7da21c06e3244a7a665ae
dylan <cdylan127@gmail.com>
2022-04-26 15:01:04 -0700

    Add a UI to the game

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # rock-paper-scissors
+
+Simple implementation of a rock, paper, scissors style game utilizing HTML, CSS, and JS.
+
+This is my first HTML/CSS/JS project that helped me learn how each language interacts with one another.
+
+The first stage | Game in the browser console
+This stage involved implementing basic functionality with JavaScript.
+
+The second stage | Game with added UI
+This stage involved adding a UI to make the game playable on a UI rather than simply in the console.
--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-const options = ["rock", "paper", "scissors"];
+// DOM selections
+const playBtn = document.querySelector(".btn-play");
+const mainPage = document.querySelector(".main-page");
+const gamePage = document.querySelector(".game-page");
+const optionsContainer = document.querySelector(".game-options");
+const gameResult = document.querySelector(".game-result");
+const gameScore = document.querySelector(".game-score");
+
+// Game variables
+const options = ["Fire", "Water", "Grass"];
+let playerSelection = "";
+let score = [0, 0];
+let gameOver = false;
+const computerPlay = function () {
+  const choice = Math.floor(Math.random() * 3);
+  return options[choice];
+};
+
+const player = function (elem) {
+  if (elem.classList.contains("btn-fire")) {
+    return "Fire";
+  }
+  if (elem.classList.contains("btn-water")) {
+    return "Water";
+  }
+  if (elem.classList.contains("btn-leaf")) {
+    return "Grass";
+  }
+};
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return `${playerSelection} vs ${computerSelection}, it's a tie!`;
+    return `${playerSelection} vs ${computerSelection}. It's a tie!`;
   } else if (
-    (playerSelection === "rock" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "scissors") ||
-    (playerSelection === "scissors" && computerSelection === "rock")
+    (playerSelection === "Fire" && computerSelection === "Water") ||
+    (playerSelection === "Water" && computerSelection === "Grass") ||
+    (playerSelection === "Grass" && computerSelection === "Fire")
   ) {
     return `You lose! ${playerSelection} loses to ${computerSelection}!`;
   } else {
@@ -14,23 +42,37 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-const computerPlay = function () {
-  const choice = Math.floor(Math.random() * 3);
-  return options[choice];
+const displayResult = function (result) {
+  if (gameOver) return;
+  if (result.includes("win")) {
+    score[0]++;
+  } else if (result.includes("lose")) {
+    score[1]++;
+  }
+  if (score[0] === 5 || score[1] === 5) {
+    gameOver = true;
+    optionsContainer.removeEventListener("click", game);
+    const res = score[0] === 5 ? "won" : "lost";
+    return `You ${res}! The final score is ${score[0]}-${score[1]}.`;
+  }
+  return `The score is ${score[0]}-${score[1]}`;
 };
 
-const play = function () {
-  let score = [0, 0];
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-    const result = playRound(playerSelection, computerPlay());
-    if (result.includes("win")) {
-      score[0]++;
-    } else if (result.includes("lose")) {
-      score[1]++;
-    }
-    console.log(result);
-    console.log(`The score is ${score[0]}-${score[1]}`);
+const game = function (e) {
+  if (
+    e.target.classList.contains("fa-solid") ||
+    e.target.firstElementChild.classList.contains("fa-solid")
+  ) {
+    const playerSelection = player(e.target.closest(".btn-game"));
+    gameResult.textContent = playRound(playerSelection, computerPlay());
+    gameScore.textContent = displayResult(gameResult.textContent);
   }
 };
-play();
+
+// Event Listeners
+playBtn.addEventListener("click", function () {
+  mainPage.style.display = "none";
+  gamePage.style.display = "flex";
+});
+
+optionsContainer.addEventListener("click", game);
